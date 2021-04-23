983e3edf9a0cd9ebeecd9f2ad61d1f6a886ef146
Justin Adams <justin@DESKTOP-GP5T8ES.localdomain>
2021-04-23 11:37:33 -0500

    completed new city state assignment

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,4 +1,4 @@
-import { getWalkers } from "./database.js"
+import { getWalkers, getCityList } from "./database.js"
 document.addEventListener(
     "click",
     (clickEvent) => {
@@ -7,17 +7,18 @@ document.addEventListener(
             const [,walkerId] = itemClicked.id.split("--")
 
             for (const walker of walkers ) {
-                if (walker.id === parseInt(walkerId)) {
-                    window.alert(`${walker.name} services ${walker.city}`)
+                for (const city of cities) {
+                if (walker.id === parseInt(walkerId) && walker.cityId === parseInt(city.id)) {
+                    window.alert(`${walker.name} services ${city.city}`)
                 }
-                
+            }
             }
         }
     }
 )
 
 const walkers = getWalkers()
-
+const cities = getCityList()
 
 export const Walkers = () => {
     let walkerHTML = "<ul>"
